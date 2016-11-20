f621754f221ae9ba070dab341f5e43c34a7f66a7
Laura Jackson <lm185074@gmail.com>
2016-11-20 14:11:19 -0500

    true kickoff

---
+++ app/app.component.ts
@@ -0,0 +1,6 @@
+import { Component, Type } from '@angular/core';
+@Component({
+    selector: 'my-app',
+    template: '<h1>Hello Again Angular!</h1>'
+})
+export class AppComponent extends Type { }
